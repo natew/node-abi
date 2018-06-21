47bfe1f3bd8972ea59b0054e40ba2866ad453a14
Nate Wienert <natewienert@gmail.com>
2018-06-21 10:39:16 -0700

    Support electron 3.0.0-beta1

--- index.js
+++ index.js
@@ -64,7 +64,8 @@ var supportedTargets = [
   {runtime: 'electron', target: '1.6.0', abi: '53', lts: false},
   {runtime: 'electron', target: '1.7.0', abi: '54', lts: false},
   {runtime: 'electron', target: '1.8.0', abi: '57', lts: false},
-  {runtime: 'electron', target: '2.0.0', abi: '57', lts: false}
+  {runtime: 'electron', target: '2.0.0', abi: '57', lts: false},
+  {runtime: 'electron', target: '3.0.0-beta1', abi: '66', lts: false}
 ]
 
 var additionalTargets = [
